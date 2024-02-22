802b4407a857d595a942a97e262002ace4605393
BananaXD <76910847+BananaXD@users.noreply.github.com>
2024-02-22 11:08:05 +0200

    Change to static

--- UnicodeNikud/Nikud.cs
+++ UnicodeNikud/Nikud.cs
@@ -1,22 +1,22 @@
 ﻿namespace UnicodeHebrew {
-    public class Nikud {
-        public char SHEVA = '\u05B0';
-        public char HATAF_SEGOL = '\u05B1';
-        public char HATAF_PATAH = '\u05B2';
-        public char HATAF_QAMATS = '\u05B3';
-        public char HIRIQ = '\u05B4';
-        public char TSERE = '\u05B5';
-        public char SEGOL = '\u05B6';
-        public char PATAH = '\u05B7';
-        public char QAMATS = '\u05B8';
-        public char HOLAM = '\u05B9';
-        public char HOLAM_HASER_FOR_VAV = '\u05BA';
-        public char QUBUTS = '\u05BB';
-        public char DAGESH_OR_MAPIQ = '\u05BC';
-        public char METEG = '\u05BD';
-        public char RAFE = '\u05BF';
-        public char SHIN_DOT = '\u05C1';
-        public char SIN_DOT = '\u05C2';
-        public char QAMATS_QATAN = '\u05C7';
+    public static class Nikud {
+        public static char SHEVA = '\u05B0';
+        public static char HATAF_SEGOL = '\u05B1';
+        public static char HATAF_PATAH = '\u05B2';
+        public static char HATAF_QAMATS = '\u05B3';
+        public static char HIRIQ = '\u05B4';
+        public static char TSERE = '\u05B5';
+        public static char SEGOL = '\u05B6';
+        public static char PATAH = '\u05B7';
+        public static char QAMATS = '\u05B8';
+        public static char HOLAM = '\u05B9';
+        public static char HOLAM_HASER_FOR_VAV = '\u05BA';
+        public static char QUBUTS = '\u05BB';
+        public static char DAGESH_OR_MAPIQ = '\u05BC';
+        public static char METEG = '\u05BD';
+        public static char RAFE = '\u05BF';
+        public static char SHIN_DOT = '\u05C1';
+        public static char SIN_DOT = '\u05C2';
+        public static char QAMATS_QATAN = '\u05C7';
     }
 }
