4b020a74690590f543fc7161cfca26abd6f8564f
BananaXD <76910847+BananaXD@users.noreply.github.com>
2024-02-22 10:52:17 +0200

    Added Nikud

---
+++ UnicodeNikud/Nikud.cs
@@ -0,0 +1,22 @@
+﻿namespace UnicodeHebrew {
+    public class Nikud {
+        public char SHEVA = '\u05B0';
+        public char HATAF_SEGOL = '\u05B1';
+        public char HATAF_PATAH = '\u05B2';
+        public char HATAF_QAMATS = '\u05B3';
+        public char HIRIQ = '\u05B4';
+        public char TSERE = '\u05B5';
+        public char SEGOL = '\u05B6';
+        public char PATAH = '\u05B7';
+        public char QAMATS = '\u05B8';
+        public char HOLAM = '\u05B9';
+        public char HOLAM_HASER_FOR_VAV = '\u05BA';
+        public char QUBUTS = '\u05BB';
+        public char DAGESH_OR_MAPIQ = '\u05BC';
+        public char METEG = '\u05BD';
+        public char RAFE = '\u05BF';
+        public char SHIN_DOT = '\u05C1';
+        public char SIN_DOT = '\u05C2';
+        public char QAMATS_QATAN = '\u05C7';
+    }
+}
